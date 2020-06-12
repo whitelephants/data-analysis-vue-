0330160f12217fbb8a6e33376fcf75b00a4c7858
wuzebin <wuzebin@sh.chinamobile.com>
2020-06-12 15:27:31 +0800

    删除antdesign包依赖

--- src/main.js
+++ src/main.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
-Vue.use(Antd);
-
 import store from './store'
 
 import Vuex from 'vuex';
